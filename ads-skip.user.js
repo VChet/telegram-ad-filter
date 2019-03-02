e7df5a37ea868e59c8ef59aa90b16377195a665f
VChet <17050347+VChet@users.noreply.github.com>
2019-03-03 04:13:56 +0500

    Refactor

--- ads-skip.user.js
+++ ads-skip.user.js
@@ -8,6 +8,20 @@
 // ==/UserScript==
 "use strict";
 
+const startScript = () => {
+  let messages = document.querySelectorAll(".im_history_message_wrap");
+  if (messages.length === messagesLength || messages.length === 0) return;
+  messagesLength = messages.length;
+  console.log({ messagesLength });
+  messages.forEach(message => {
+    if (adWords.some(v => message.innerText.indexOf(v) >= 0 )) {
+      message = message.querySelector(".im_message_body");
+      message.classList.add("advertisementMessage");
+      message.onclick = () => message.classList.toggle("advertisementMessage");
+    }
+  });
+}
+
 const eventThrottler = delay => {
   if (!eventTimeout) {
     eventTimeout = setTimeout(() => {
@@ -17,31 +31,16 @@ const eventThrottler = delay => {
   }
 };
 
-const startScript = () => {
-  let messages = document.querySelectorAll(".im_history_message_wrap");
-  if (messages.length === messagesLength) return;
-  if (messages.length === 0) return;
-  messagesLength = messages.length;
-  console.log({ messagesLength });
-  messages.forEach(message => {
-    const adWords = [
-      "#взаимопиар",
-      "#партнерский",
-      "#постпроплачен",
-      "#реклама",
-      "#рекламныйпост",
-      "#текстприслан",
-    ];
-    if (adWords.some(v => message.innerText.indexOf(v) >= 0 )) {
-      message = message.querySelector(".im_message_body");
-      message.classList.add("advertisementMessage");
-      message.onclick = () => message.classList.toggle("advertisementMessage");
-    }
-  });
-}
-
 GM_addStyle(".advertisementMessage {color:dodgerblue!important; max-height:40px;}");
 
+const adWords = [
+  "#взаимопиар",
+  "#партнерский",
+  "#постпроплачен",
+  "#реклама",
+  "#рекламныйпост",
+  "#текстприслан",
+];
 let messagesLength;
 let eventTimeout;
 let delay = 3000;
