dc0cc429f81b6fda0c05e6bd365499792fbbb7d9
VChet <17050347+VChet@users.noreply.github.com>
2019-07-15 21:43:04 +0500

    0.3.1

--- tg-ad-filter.user.js
+++ tg-ad-filter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Telegram Ad Filter
-// @version     0.3.0
+// @version     0.3.1
 // @description Collapses messages that contain words from the ad-word list
 // @license     MIT
 // @author      VChet
