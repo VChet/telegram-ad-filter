6df423c715c55786e2cf0c05b6bc44dc778b6d4f
VChet <17050347+VChet@users.noreply.github.com>
2021-09-11 19:08:46 +0500

    0.4.1

--- tg-ad-filter.user.js
+++ tg-ad-filter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Telegram Ad Filter
-// @version     0.4.0
+// @version     0.4.1
 // @description Collapses messages that contain words from the ad-word list
 // @license     MIT
 // @author      VChet
@@ -33,12 +33,8 @@
     node.querySelector(".bubble-content").prepend(trigger);
 
     node.classList.add("hasAdvertisement");
-    trigger.addEventListener("click", () => {
-      node.classList.remove("hasAdvertisement");
-    });
-    message.addEventListener("click", () => {
-      node.classList.add("hasAdvertisement");
-    });
+    trigger.addEventListener("click", () => { node.classList.remove("hasAdvertisement"); });
+    message.addEventListener("click", () => { node.classList.add("hasAdvertisement"); });
   }
 
   GM_addStyle(`
@@ -59,10 +55,7 @@
 
   GM_registerMenuCommand("Filter list", () => {
     const oldValue = GM_getValue("ad-words", adWords);
-    const input = prompt(
-      "Enter words to filter, separated by comma:",
-      oldValue
-    );
+    const input = prompt("Enter words to filter, separated by comma:", oldValue);
     // Convert string to array, trim values
     const newValue = input.split(",").reduce((acc, entry) => {
       if (entry) acc.push(entry.trim());
@@ -70,25 +63,17 @@
     }, []);
     adWords = newValue;
     GM_setValue("ad-words", newValue);
-    document.querySelectorAll(".bubble").forEach((message) => {
-      applyStyles(message);
-    });
+    document.querySelectorAll(".bubble").forEach((message) => { applyStyles(message); });
   });
 
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.type !== "childList" || !mutation.addedNodes.length) return;
       mutation.addedNodes.forEach((node) => {
-        if (node.nodeType === 1 && node.classList.contains("bubble")) {
-          applyStyles(node);
-        }
+        if (node.nodeType === 1 && node.classList.contains("bubble")) applyStyles(node);
       });
     });
   });
 
-  observer.observe(document.documentElement, {
-    childList: true,
-    subtree: true,
-    attributeFilter: ["class"],
-  });
-})();
+  observer.observe(document.documentElement, { childList: true, subtree: true, attributeFilter: ["class"] });
+}());
