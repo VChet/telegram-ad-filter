756c66df0fd684211e5596ae92769c18f68bd0f2
VChet <17050347+VChet@users.noreply.github.com>
2024-02-04 23:04:41 +0400

    chore: bump metadata version

--- src/bump.cjs
+++ src/bump.cjs
@@ -1,19 +1,19 @@
 const { join } = require("node:path");
-const { readdir, readFile, writeFile } = require("node:fs/promises");
+const { readFile, writeFile } = require("node:fs/promises");
 const { execSync } = require("node:child_process");
 
 const root = join(__dirname, "..");
+const files = [join(root, "tg-ad-filter.user.js"), join(root, "src/meta.txt")];
 
 async function updateFiles() {
   try {
-    const files = await readdir(root);
-    const userJSFilename = files.find(file => file.endsWith(".user.js"));
-    const filepath = join(root, userJSFilename);
-    const contents = await readFile(filepath, "utf8");
-    const updatedContents = contents.replace(/\b\d+\.\d+\.\d+\b/g, process.env.npm_package_version);
-    await writeFile(filepath, updatedContents, "utf8");
+    for (const filepath of files) {
+      const contents = await readFile(filepath, "utf8");
+      const updatedContents = contents.replace(/\b\d+\.\d+\.\d+\b/g, process.env.npm_package_version);
+      await writeFile(filepath, updatedContents, "utf8");
+    }
 
-    execSync(`git add ${filepath}`).toString();
+    execSync(`git add ${files.join(" ")}`).toString();
   } catch (error) {
     console.error("Error:", error);
   }
