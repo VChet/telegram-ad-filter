3cd07663cb3619a34de3afff9053761c38990a9a
VChet <17050347+VChet@users.noreply.github.com>
2025-02-07 02:18:42 +0500

    1.2.0

--- tg-ad-filter.user.js
+++ tg-ad-filter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Telegram Ad Filter
-// @version      1.1.4
+// @version      1.2.0
 // @description  Collapses messages that contain words from the ad-word list
 // @license      MIT
 // @author       VChet
@@ -77,7 +77,7 @@ function addSettingsButton(node, callback) {
   ripple.classList.add("c-ripple");
   const icon = document.createElement("span");
   icon.classList.add("tgico", "button-icon");
-  icon.textContent = "\uE9DE";
+  icon.textContent = "\uE9F3";
   settingsButton.append(ripple);
   settingsButton.append(icon);
 
@@ -119,15 +119,51 @@ const settingsConfig = {
   }
 };
 
+function isValidURL(url) {
+  try {
+    const parsedUrl = new URL(url);
+    return parsedUrl.protocol === "http:" || parsedUrl.protocol === "https:";
+  } catch {
+    return false;
+  }
+}
+
+function isValidJSON(payload) {
+  try {
+    JSON.parse(payload);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function fetchAndParseJSON(url) {
+  const content = await fetch(url).then(response => response.text());
+  if (!isValidJSON(content)) { throw new SyntaxError(`Invalid JSON: data from ${url}`); }
+  return JSON.parse(content);
+}
+
 async function fetchLists(urlsString) {
-  const urls = urlsString.trim().split("\n");
-  const lists = await Promise.all(
-    urls.map((url) => fetch(url)
-      .then((response) => response.json())
-      .catch((error) => alert(error))
-    )
-  );
-  return [...new Set(lists.flat())];
+  const urls = urlsString.trim().split("\n").map(url => url.trim()).filter(Boolean);
+  const resultSet = new Set();
+
+  for (const url of urls) {
+    if (!isValidURL(url)) {
+      throw new URIError(`Invalid URL: ${url}. Please ensure it leads to an online source like GitHub, Gist, Pastebin, etc.`);
+    }
+
+    try {
+      let parsedData = await fetchAndParseJSON(url);
+      if (!Array.isArray(parsedData)) { throw new TypeError(`Invalid array: data from ${url}`); }
+      parsedData = parsedData.map(entry => entry.trim()).filter(Boolean);
+      for (const entry of parsedData) { resultSet.add(entry); }
+    } catch (error) {
+      if (error instanceof SyntaxError) { throw error; }
+      throw new Error(`Fetch error: ${url}. Please check the URL or your network connection.`);
+    }
+  }
+
+  return [...resultSet];
 }
 
 (async() => {
@@ -139,8 +175,12 @@ async function fetchLists(urlsString) {
     events: {
       init: async function() { adWords = await fetchLists(this.get("listUrls")); },
       save: async function() {
-        adWords = await fetchLists(this.get("listUrls"));
-        this.close();
+        try {
+          adWords = await fetchLists(this.get("listUrls"));
+          this.close();
+        } catch (error) {
+          alert(error.message);
+        }
       }
     }
   });
