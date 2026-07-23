4d0c2f98ec713a59144991e2ed9e009bc06d8e92
VChet <17050347+VChet@users.noreply.github.com>
2026-07-23 15:33:41 +0500

    chore: rename entry file

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,6 @@ import { readFileSync } from "node:fs";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: "src/main.ts",
   banner: { js: readFileSync("src/meta.txt", "utf-8") },
   format: "cjs",
   outputOptions: { dir: ".", entryFileNames: "tg-ad-filter.user.js" }
