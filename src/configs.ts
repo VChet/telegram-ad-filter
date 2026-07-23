5ba6283cb5c4bf513e83e862734386a20e1d02b3
VChet <17050347+VChet@users.noreply.github.com>
2026-07-23 14:40:20 +0500

    feat(popup): add subtitle, update styles

--- src/configs.ts
+++ src/configs.ts
@@ -1,10 +1,22 @@
 import { frameStyle, popupStyle } from "./DOM";
+import pkg from "../package.json";
+
+const title = document.createElement("div");
+title.innerHTML = `
+  Telegram Ad Filter Settings
+  <p class="subtitle">
+    <a href="https://github.com/VChet/telegram-ad-filter/releases" target="_blank">v${pkg.version}</a>
+  </p>
+  <p class="subtitle">
+    Suggest new words to filter in the <a href="https://github.com/VChet/telegram-ad-filter/discussions" target="_blank">discussions</a>.
+  </p>
+`;
 
 export const settingsConfig: InitOptions<"textarea"> = {
   id: "telegram-ad-filter",
   frameStyle,
   css: popupStyle,
-  title: "Telegram Ad Filter Settings",
+  title,
   fields: {
     listUrls: {
       label: "Blacklist URLs (one per line) – each URL must be a publicly accessible JSON file containing an array of blocked words or phrases",
